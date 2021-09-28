e23887edbc0fba0b500f2fd03fc7a279f5362b70
Lucas Smith <lsoceanengineer@gmail.com>
2021-09-28 01:46:04 -0700

    Add project files.

---
+++ Project 1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            double weight = Convert.ToDouble(WeightBox.Text);
+            double height = Convert.ToDouble(HeightBox.Text);
+
+            double bmi = (weight * 720) / (height * height);
+
+            if (bmi > 26)
+            {
+                BMIButton.Background = Brushes.Yellow;
+            } else if (bmi >= 15 && bmi <= 26)
+            {
+                BMIButton.Background = Brushes.Green;
+            } else if (bmi < 15)
+            {
+                BMIButton.Background = Brushes.Blue;
+            }
+        }
+    }
+}
---
+++ Project 2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 8;
+            int r, j;
+            double headCount = 0;
+            double tailCount = 0;
+            
+            Console.WriteLine("Enter 8 coin toss results");
+            char[] tosses = new char[7];
+            for (int i = 0; i < 7; i++) {
+                tosses[i] = Convert.ToChar(Console.ReadLine());
+            }
+            int n = 1;
+            foreach (char toss in tosses)
+            {
+                Console.WriteLine("Toss: " + n + ": " + toss);
+                n++;
+            }
+
+            for (j = 0; j < num; j++)
+            {
+                if (tosses[j] == 'h')
+                {
+                    headCount++;
+                    break;
+                } else if (tosses[j] == 't')
+                {
+                    tailCount++;
+                    break;
+                }
+            }
+            
+            double total = headCount + tailCount;
+            double headsPercent = headCount / total * 100;
+            double tailsPercent = tailCount / total * 100;
+
+            Console.WriteLine("Heads: " + headsPercent + "%.");
+            Console.WriteLine("Tails: " + tailsPercent + "%.");
+        }
+    }
+}
